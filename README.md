74ecd2ddc960416868c4c2da32853e16d43cebc7
Ray-Shakex <bshakex@gmail.com>
2021-04-26 11:49:58 +0200

    README and preloader logic

--- README.md
+++ README.md
@@ -17,4 +17,8 @@ then add .slide
 -else video.play();
 
 ### preloader 
--
+-The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading;
+-The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images;
+
+-.preloader is 'visible' by default and when whole page loads then we add .hide-preloader class;
+-hide-preloader class is added when through callback() in the event litener on the window. in callback function classlist is added to preloader class on "load". 
--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 // MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 
 
 const button = document.querySelector('.switch-btn');
@@ -15,4 +13,11 @@ button.addEventListener('click', () => {
         button.classList.remove('slide');
         video.play();
     }
-})
+});
+
+//preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+    preloader.classList.add('hide-preloader');
+});
